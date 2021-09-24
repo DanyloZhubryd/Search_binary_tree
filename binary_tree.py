a5b72a9947647f9bf9e5c9e809caec07195104dd
Zhubryd Danylo <zubdan21@gmail.com>
2021-09-24 10:26:21 +0300

    Adding search binary tree insert and search methods

---
+++ binary_tree.py
@@ -0,0 +1,68 @@
+class Tree:
+
+    def __init__(self):
+        self.root = None
+
+    def search(self, value):
+        found_node = self._search(self.root, value)
+        if found_node is None:
+            return False
+        return True
+
+    def _search(self, node_to_check, value):
+        if (node_to_check is None) or (node_to_check.value == value):
+            return node_to_check
+
+        if value > node_to_check.value:
+            # go right
+            return self._search(node_to_check.right, value)
+        else:
+            # go left
+            return self._search(node_to_check.left, value)
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
+        self._insert(self.root, value)
+
+    def _insert(self, current_node, value):
+        if value > current_node.value:
+            # add to the right
+            if current_node.right is None:
+                current_node.right = Node(value, current_node)
+                return
+            self._insert(current_node.right, value)
+        else:
+            # add to the left
+            if current_node.left is None:
+                current_node.left = Node(value, current_node)
+                return
+            self._insert(current_node.left, value)
+
+    def max_value(self):
+        pass
+
+    def min_value(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class Node:
+    def __init__(self, value, parent=None):
+        self.right = None
+        self.left = None
+        self.parent = parent
+        self.value = value
+
+
+tree = Tree()
+tree.insert(10)
+tree.insert(8)
+tree.insert(6)
+print(tree.search(10))
+print(tree.search(8))
+print(tree.search(6))
+tree.print_tree()
